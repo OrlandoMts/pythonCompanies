29c3f9229f114207810580d1f493bc4543a90c37
OrlandoMts <dani_monts19@hotmail.com>
2022-02-09 22:03:45 -0600

    modifique la config de la conexion sql, cree un modelo y ejecute una migracion

--- Proyecto_API/Proyecto_API/settings.py
+++ Proyecto_API/Proyecto_API/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'Proyecto_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'USER': 'root',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_api',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
