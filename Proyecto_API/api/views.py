cd4f6305a44eb7c7c0c953a86df962b8710c5290
OrlandoMts <dani_monts19@hotmail.com>
2022-02-20 16:53:25 -0600

    Escribi los metodos faltantes de la vista company

--- Proyecto_API/api/views.py
+++ Proyecto_API/api/views.py
@@ -1,34 +1,79 @@
-from asyncore import dispatcher
-from copyreg import dispatch_table
+import json
 from django.views import View
 from .models import Company
 from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 class CompanyView(View):
 
-    
+    """
+        csrf_exempt elimina el error de la verificacion del cliente
+        al realizar una peticion POST 
+        (En este ejemplo no use un formulario, use postman. Por eso el error)
+    """
 
-    def get(self, request):
-        companies = list(Company.objects.values())
-        if len(companies)>0:
-            data={
-                'message': "Success",
-                'companies': companies
-            }
+    #Este metodo se ejecuta cada vez que se hace una peticion
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id=0):
+        if(id>0):
+            companies = list(Company.objects.filter(id=id).values())
+            if len(companies) > 0:
+                company = companies[0]
+                data={
+                    'message': "Success",
+                    'company': company
+                }
+            else:
+                data={'message': "Company not found :("}
         else:
-            data={
-                'message': "Companies not found!"
-            }
+            companies = list(Company.objects.values())
+            if len(companies)>0:
+                data={
+                    'message': "Success",
+                    'companies': companies
+                }
+            else:
+                data={
+                    'message': "Companies not found!"
+                }
         return JsonResponse(data)
     
     def post(self, request):
-        pass
+        # print(request.body)
+        # Convierte el JSON a un diccionario de python
+        jd = json.loads(request.body)
+        Company.objects.create(name=jd['name'], webSite=jd['webSite'], foundation=jd['foundation'])
+        data={'message': "Success"}
+        return JsonResponse(data)
     
-    def put(self, request):
-        pass
+    def put(self, request, id):
+        jd = json.loads(request.body)
+        companies = list(Company.objects.filter(id=id).values())
+        if len(companies) > 0:
+            company = Company.objects.get(id=id)
+            company.name = jd['name']
+            company.webSite = jd['webSite']
+            company.foundation = jd['foundation']
+            company.save()
+            data={'message:' "Success, company updated :)"}
+        else:
+            data={'message': "Company not found. Can't update :("}
+        return JsonResponse(data, safe=False)
     
-    def delete(self, request):
-        pass
+    def delete(self, request, id):
+        companies = list(Company.objects.filter(id=id).values())
+
+        if len(companies) > 0:
+            Company.objects.filter(id=id).delete()
+            data={'message:' "Success"}            
+        else: 
+            data={'message': "Company not found"}
+        
+        return JsonResponse(data, safe=False)
     
