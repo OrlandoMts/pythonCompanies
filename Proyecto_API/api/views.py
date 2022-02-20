0806cb99c7b729ea791a788102b43b05f6a29aab
OrlandoMts <dani_monts19@hotmail.com>
2022-02-20 13:50:30 -0600

    Cree una vista con sus metodos http, un archivo urls.py para manejar las rutas y modifique el archivo urls.py que se creo al inicio

--- Proyecto_API/api/views.py
+++ Proyecto_API/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from asyncore import dispatcher
+from copyreg import dispatch_table
+from django.views import View
+from .models import Company
+from django.http.response import JsonResponse
 
 # Create your views here.
+
+class CompanyView(View):
+
+    
+
+    def get(self, request):
+        companies = list(Company.objects.values())
+        if len(companies)>0:
+            data={
+                'message': "Success",
+                'companies': companies
+            }
+        else:
+            data={
+                'message': "Companies not found!"
+            }
+        return JsonResponse(data)
+    
+    def post(self, request):
+        pass
+    
+    def put(self, request):
+        pass
+    
+    def delete(self, request):
+        pass
+    
